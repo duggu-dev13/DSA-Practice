a9d83486e7d58a6787b8d5129aa8768fd68eff94
Vaibhav Sonawane <work.vaibhav1308@gmail.com>
2026-06-18 20:44:22 +0530

    pattern 3

--- pattern1.cpp
+++ pattern1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int x = 6;
     for (int i = 0; i < x; i++) {
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < x; j++) {
             cout << "*";
         }
         cout << "\n";
